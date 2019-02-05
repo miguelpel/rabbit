e350a20be4980038b89c5fafe7faa6dba6755654
miguelpel <pelleteratdeborde@yahoo.fr>
2019-02-05 18:29:35 +0200

    first commit, planning

---
+++ index.js
@@ -0,0 +1,11 @@
+// create an array from the wordlist
+
+// filter and remove from his array every entry that is not contained in original anagram
+
+// give the resulting array to an algorythm (three nested loops?) that returns complete anagrams.
+
+// Hash those anagrams against the three hashes, and return the designated anagrams.
+
+// console.log(`easiest secret phrase: '${sentence1}'`)
+// console.log(`more difficult secret phrase: '${sentence2}'`)
+// console.log(`hard secret phrase: '${sentence3}'`)
